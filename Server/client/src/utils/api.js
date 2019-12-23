66ab12f2a3a1d70e43a0c2182b0db73630bf768e
JackRyan1989 <jack.jackryan@gmail.com>
2019-12-23 14:20:07 -0500

    Backend prep is about as far as I can take it now. I should build out the front-end to get a better idea of what I need to do.

--- Server/client/src/utils/api.js
+++ Server/client/src/utils/api.js
@@ -1,1 +1,14 @@
-//Build this out so it does all of the querying of google books
+import axios from 'axios';
+import dotenv from 'dotenv';
+const BASEURL = 'https://www.googleapis.com/books/v1/volumes?q=';
+const key = dotenv.key;
+
+
+//Query Google Books API to search for book:
+export default {
+    findBooks: function(query) {
+        return axios.get(BASEURL + query + key);
+        }
+    };
+
+//We will need to pass other routes through here,right?
