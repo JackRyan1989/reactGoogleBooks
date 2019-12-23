2e7523b0a1d925246026eaa34623d61820af0d35
JackRyan1989 <jack.jackryan@gmail.com>
2019-12-23 17:02:03 -0500

    Added no match page condition.

--- Server/client/src/App.js
+++ Server/client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Library from "./pages/library";
 import Shelf from "./pages/shelf";
+import NoMatch from "./pages/noMatch";
 import './App.css';
 
 //Main app will handle switching between pages, and that's about it.
@@ -13,7 +14,7 @@ function App() {
         <Switch>
             <Route exact path='/' component={Library}/>
             <Route exact path='/books' component={Shelf}/>
-            {/* <Route component={NoMatch}/> */}
+            <Route component={NoMatch}/>
         </Switch>
       </div>
     </Router>
---
+++ Server/client/src/pages/noMatch.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NoMatch() {
+    return(
+        <div>No match works!</div>
+    );
+}
+
+export default NoMatch
