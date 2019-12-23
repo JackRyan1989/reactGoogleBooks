23950c4c85aceb01ff0f90c28f69839528efdfbb
JackRyan1989 <jack.jackryan@gmail.com>
2019-12-23 12:30:31 -0500

    Model is done, dbSeed is done, Routes need to be built, Controller needs to be built, server.js needs to be fleshed out.

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -3,3 +3,14 @@ const router = require('express').router;
 const apiRoutes = require('./api');
 
 //Tell the Express router to use the api routes:
+router.use('/api', apiRoutes);
+
+//If no api routes are hit, send the React app:
+//This makes sense because when we initially launch the app, we won't hit any
+//API routes. The API routes should only be hit inside of the application.
+//Note for this to work, I think the application needs to be fully built?
+router.use(function(req, res){
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
